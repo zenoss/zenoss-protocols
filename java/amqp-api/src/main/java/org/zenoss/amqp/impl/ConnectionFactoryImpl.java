ac013ebccac66dd44591a74146f1d316ab60ff4f
dgarcia <dgarcia@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-11-14 10:22:05 +0000

    * fixes 4199: allow configuration of amqp heartbeat in ZEP; reviewed by pwarren.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/protocols@66067 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- java/amqp-api/src/main/java/org/zenoss/amqp/impl/ConnectionFactoryImpl.java
+++ java/amqp-api/src/main/java/org/zenoss/amqp/impl/ConnectionFactoryImpl.java
@@ -12,12 +12,68 @@ package org.zenoss.amqp.impl;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zenoss.amqp.AmqpException;
 import org.zenoss.amqp.AmqpServerUri;
 import org.zenoss.amqp.Connection;
 import org.zenoss.amqp.ConnectionFactory;
 
+import java.io.*;
+
 public class ConnectionFactoryImpl extends ConnectionFactory {
+
+    private static final Logger logger = LoggerFactory
+            .getLogger(ConnectionFactoryImpl.class.getName());
+
+    private Integer heartbeat = null;
+
+    private int getHeartbeat() {
+
+        if (this.heartbeat != null) {
+            return (int)this.heartbeat;
+        }
+
+        // set the default heartbeat up
+        this.heartbeat = 0;
+
+        FileInputStream fstream = null;
+        BufferedReader br = null;
+        try {
+            String line;
+            String filename = String.format("%s/etc/global.conf", System.getenv("ZENHOME"));
+
+            fstream = new FileInputStream(filename);
+            br = new BufferedReader(new InputStreamReader(fstream));
+            while((line = br.readLine()) != null) {
+                if (line.startsWith("amqpheartbeat")) {
+                    String[] parts = line.split("\\s+");
+                    if (parts.length >=2) {
+                        Integer myheartbeat = Integer.valueOf(parts[1]);
+                        if (myheartbeat != null) {
+                            if (myheartbeat >= 0) {
+                                this.heartbeat = myheartbeat;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+        } catch (Exception ex) {
+            logger.warn("There was a problem reading amqpconnectionheartbeat from $ZENHOME/etc/global.conf: {}", ex.toString());
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {}
+            } else if (fstream != null)
+                try {
+                    fstream.close();
+                } catch (IOException ex) {}
+        }
+        return (int)this.heartbeat;
+    }
 
     @Override
     public Connection newConnection(AmqpServerUri uri) throws AmqpException {
@@ -41,7 +97,14 @@ public class ConnectionFactoryImpl extends ConnectionFactory {
         // TODO: Support these parameters via AmqpServerUri?
         // factory.setRequestedChannelMax(?);
         // factory.setRequestedFrameMax(?);
-        // factory.setRequestedHeartbeat(?);
+
+        int myhearbeat = this.getHeartbeat();
+        if (myhearbeat > 0) {
+            logger.info("Setting amqpconnectionheartbeat to {}", myhearbeat);
+            factory.setRequestedHeartbeat(myhearbeat);
+        } else {
+            logger.info("No amqpconnectionheartbeat on AMQP connection");
+        }
         // factory.setClientProperties(?);
         try {
             return new ConnectionImpl(factory.newConnection());
