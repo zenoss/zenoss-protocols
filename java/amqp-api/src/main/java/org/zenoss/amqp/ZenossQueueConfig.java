7510a91edb827a7c3b64937ee97876648d47112a
pwarren <pwarren@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2013-01-31 18:36:31 +0000

    Add some logging to ZenossQueueConfig to help diagnose issues (instead of ignoring them). Make the inability to read messaging.conf non-fatal. Avoid a race condition when initializing the configuration. Reviewed by pbowman.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/protocols@69038 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- java/amqp-api/src/main/java/org/zenoss/amqp/ZenossQueueConfig.java
+++ java/amqp-api/src/main/java/org/zenoss/amqp/ZenossQueueConfig.java
@@ -10,11 +10,14 @@
 
 package org.zenoss.amqp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zenoss.utils.ZenPacks;
 import org.zenoss.utils.Zenoss;
 import org.zenoss.utils.ZenossException;
 
 import java.io.BufferedInputStream;
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -26,7 +29,18 @@ import java.util.List;
  * Helper class used to retrieve the {@link QueueConfig} object for Zenoss.
  */
 public class ZenossQueueConfig {
+    private static final Logger logger = LoggerFactory.getLogger(ZenossQueueConfig.class);
     private static volatile QueueConfig sZenossQueueConfig = null;
+
+    private static void close(Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+                logger.warn("Failed to close {}: {}", closeable, e.getLocalizedMessage());
+            }
+        }
+    }
 
     /**
      * Returns the {@link QueueConfig} object for merged .qjs files, including those
@@ -37,48 +51,56 @@ public class ZenossQueueConfig {
      */
     public static QueueConfig getConfig() throws IOException {
         if (sZenossQueueConfig == null) {
-            List<InputStream> streams = new ArrayList<InputStream>();
-            try {
-                InputStream is = ZenossQueueConfig.class.getResourceAsStream("/src/zenoss.qjs");
-                if (is != null) {
-                    streams.add(is);
+            synchronized (ZenossQueueConfig.class) {
+                if (sZenossQueueConfig == null) {
+                    sZenossQueueConfig = loadConfig();
                 }
-                try {
-                    for (String path : ZenPacks.getQueueConfigPaths()) {
-                        streams.add(new BufferedInputStream(new FileInputStream(path)));
-                    }
-                } catch (ZenossException ignored) {
-                    // Don't load from ZenPacks, I guess
-                }
-                sZenossQueueConfig = new QueueConfig(streams);
-            } finally {
-                for (InputStream is : streams) {
-                    if (is != null) {
-                        try {
-                            is.close();
-                        } catch (IOException ignored) {
-                        }
-                    }
-                }
-            }
-            try {
-                InputStream msgConfStream = null;
-                File confFile = new File(Zenoss.zenPath("etc", "messaging.conf"));
-                try {
-                    if (confFile.isFile()) {
-                        msgConfStream = new BufferedInputStream(
-                                new FileInputStream(confFile));
-                        sZenossQueueConfig.loadProperties(msgConfStream);
-                    }
-                } finally {
-                    if (msgConfStream != null) {
-                        msgConfStream.close();
-                    }
-                }
-            } catch (ZenossException ignored) {
-                // Everybody gots problems, yo. Nothing to feel bad about. Accept them and move on.
             }
         }
         return sZenossQueueConfig;
+    }
+
+    private static QueueConfig loadConfig() throws IOException {
+        QueueConfig queueConfig = null;
+        List<InputStream> streams = new ArrayList<InputStream>();
+        try {
+            InputStream is = ZenossQueueConfig.class.getResourceAsStream("/src/zenoss.qjs");
+            if (is != null) {
+                streams.add(is);
+            }
+            try {
+                for (String path : ZenPacks.getQueueConfigPaths()) {
+                    try {
+                        streams.add(new BufferedInputStream(new FileInputStream(path)));
+                    } catch (IOException e) {
+                        logger.warn("Failed to load ZenPack queue configuration from {}: {}", path,
+                                e.getLocalizedMessage());
+                    }
+                }
+            } catch (ZenossException e) {
+                logger.warn("Failed to determine paths to ZenPack queue configuration: {}",
+                        e.getLocalizedMessage());
+            }
+            queueConfig = new QueueConfig(streams);
+        } finally {
+            for (InputStream is : streams) {
+                close(is);
+            }
+        }
+        InputStream msgConfStream = null;
+        try {
+            File confFile = new File(Zenoss.zenPath("etc", "messaging.conf"));
+            if (confFile.isFile()) {
+                msgConfStream = new BufferedInputStream(new FileInputStream(confFile));
+                queueConfig.loadProperties(msgConfStream);
+            }
+        } catch (ZenossException e) {
+            logger.warn("Failed to determine path to messaging.conf: {}", e.getLocalizedMessage());
+        } catch (IOException e) {
+            logger.warn("Failed to load messaging.conf configuration: {}", e.getLocalizedMessage());
+        } finally {
+            close(msgConfStream);
+        }
+        return queueConfig;
     }
 }
