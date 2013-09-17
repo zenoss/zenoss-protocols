00f963bcc5a8ae73373901d88e1e86f67fa4168e
jplouis <jplouis@gmail.com>
2013-09-17 14:55:01 -0500

    add 'a' to version to differentiate release status

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zenoss.protocols",
-    version = "1.0.0",
+    version = "1.0.0a",
     packages = find_packages(),
     long_description=open('README.txt').read(),
     install_requires = [
