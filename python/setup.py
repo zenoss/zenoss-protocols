737d6c4979faf4da81487b99e3cab27c04ef46fb
Glenn Jones <gjones@zenoss.com>
2015-03-26 14:42:42 -0500

    Bump release to 2.1.1

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zenoss.protocols",
-    version = "2.1.1dev",
+    version = "2.1.1",
     packages = find_packages(),
     long_description=open('README.txt').read(),
     install_requires = [
