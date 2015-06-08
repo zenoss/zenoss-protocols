5784b4bf8cf36f9e889505150fcd506ba555fec3
Ian McCracken <ian.mccracken@gmail.com>
2015-06-08 09:49:46 -0500

    Bump versions to 2.1.3-SNAPSHOT

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zenoss.protocols",
-    version = "2.1.2",
+    version = "2.1.3dev",
     packages = find_packages(),
     long_description=open('README.txt').read(),
     install_requires = [
