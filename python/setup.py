d721bb03f46bfc4e132e6432fe7d681d042092f3
Eric Miller <sockterrier@gmail.com>
2016-01-21 16:09:54 -0600

    releasing 2.1.4

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zenoss.protocols",
-    version = "2.1.4dev",
+    version = "2.1.4",
     packages = find_packages(),
     long_description=open('README.txt').read(),
     install_requires = [
