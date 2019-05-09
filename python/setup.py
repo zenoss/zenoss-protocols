45c8ba420e48f9ea90f47e737563541b784557dc
Paul Johnston <pjohnston@zenoss.com>
2019-05-09 09:20:40 -0500

    Revert "ZEN-31738: Change default event archive retentions to 450 days"

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zenoss.protocols",
-    version = "3.0.0-dev",
+    version = "2.1.9-dev",
     packages = find_packages(),
     long_description=open('README.txt').read(),
     install_requires = [
